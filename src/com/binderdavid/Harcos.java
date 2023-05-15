d601977f1c9a4c42da472f11546b018e2a7ae905
Binder Dávid Gábor <binder.david.gabor@gmail.com>
2023-05-15 12:19:34 +0200

    Harcos osztály létrehozása

---
+++ src/com/binderdavid/Harcos.java
@@ -0,0 +1,12 @@
+package com.binderdavid;
+
+public class Harcos extends Karakter {
+    public Harcos() {
+        super();
+    }
+
+    @Override
+    public String getNev() {
+        return "Harcos";
+    }
+}
