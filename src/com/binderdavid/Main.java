9d2129d951ac494c609ab6cd5be85b4286cb68d7
Binder Dávid Gábor <binder.david.gabor@gmail.com>
2023-05-15 13:18:51 +0200

    Main StringBuilder  alkalmazása

--- src/com/binderdavid/Main.java
+++ src/com/binderdavid/Main.java
@@ -4,41 +4,66 @@ public class Main {
 
     public static void main(String[] args) {
 
-        JatekTer jatekTer=new JatekTer();
-        Karakter[] karakterek=jatekTer.getKarakterek();
+        JatekTer jatekTer = new JatekTer();
+        Karakter[] karakterek = jatekTer.getKarakterek();
 
-        int kor=1;
-        boolean veg=false;
+        //int kor=1;
+        boolean veg = false;
 
-        while (!veg){
+        while (!veg) {
 
-            System.out.println("Kor " + kor + ": " + karakterek[0].getNev() + ":" + karakterek[0].getEletero()
-                    + ", " + karakterek[1].getNev() + ":" + karakterek[1].getEletero());
-            if (karakterek[0].getPozicio()==karakterek[1].getPozicio()){
+            System.out.println(new StringBuilder()
+
+                    .append(karakterek[0].getNev())
+                    .append("_")
+                    .append(karakterek[1].getNev())
+                    .append("-->")
+                    .append(karakterek[0].getNev())
+                    .append(" :")
+                    .append(karakterek[0].getEletero()).append(", ")
+                    .append(karakterek[1].getNev())
+                    .append(":")
+                    .append(karakterek[1].getEletero()).toString());
+
+            if (karakterek[0].getPozicio() == karakterek[1].getPozicio()) {
 
 
-                int sebzes= (int) (Math.random() * 6 +1);
+                int sebzes = (int) (Math.random() * 6 + 1);
                 karakterek[1].serul(sebzes);
-                System.out.println("Harc: " + karakterek[0].getNev() + ":" + karakterek[0].getEletero() + ", "
-                        + karakterek[1].getNev() + ":" + karakterek[1].getEletero());
-            }
-            if(karakterek[1].getEletero()>0){
+                System.out.println(new StringBuilder()
+                        .append("X__ --> harc: ")
+                        .append(karakterek[0].getNev())
+                        .append(":")
+                        .append(karakterek[0].getEletero())
+                        .append(", ")
+                        .append(karakterek[1].getNev())
+                        .append(":")
+                        .append(karakterek[1].getEletero()).toString());
+                if (karakterek[1].getEletero() > 0) {
 
-                int ellenCsapas=(int) (Math.random() * 6 +1);
-                karakterek[0].serul(ellenCsapas);
-                System.out.println("Harc: " + karakterek[0].getNev() + ":" + karakterek[0].getEletero() + ", "
-                        + karakterek[1].getNev() + ":" + karakterek[1].getEletero());
+                    int ellenCsapas = (int) (Math.random() * 6 + 1);
+                    karakterek[0].serul(ellenCsapas);
+                    System.out.println(new StringBuilder()
+                            .append("X__ --> harc: ")
+                            .append(karakterek[0].getNev())
+                            .append(":")
+                            .append(karakterek[0].getEletero())
+                            .append(", ")
+                            .append(karakterek[1].getNev())
+                            .append(":")
+                            .append(karakterek[1].getEletero()).toString());
+
+                }
+                if (karakterek[0].getEletero() <= 0 || karakterek[1].getEletero() <= 0) {
+                    veg = true;
+                    System.out.println("A jatek veget ert!");
+                }
 
             }
-            if (karakterek[0].getEletero() <= 0 || karakterek[1].getEletero() <= 0) {
-                veg = true;
-                System.out.println("A jatek veget ert!");
-            }
-            else{
+            else {
                 karakterek[0].lep();
                 karakterek[1].lep();
             }
-            kor++;
 
         }
     }
