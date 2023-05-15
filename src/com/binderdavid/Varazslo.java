bab6fbf493f40d5141e3139000fe6a312b8ac9ee
Binder Dávid Gábor <binder.david.gabor@gmail.com>
2023-05-15 12:20:53 +0200

    Varázsló osztály létrehozása

---
+++ src/com/binderdavid/Varazslo.java
@@ -0,0 +1,12 @@
+package com.binderdavid;
+
+public class Varazslo extends Karakter {
+    public Varazslo() {
+        super();
+    }
+
+    @Override
+    public String getNev() {
+        return "Varázsló";
+    }
+}
