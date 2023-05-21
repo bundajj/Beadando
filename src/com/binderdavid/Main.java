5d6041920bbab0e6bfcc50dce0b3880cf5565997
Binder Dávid Gábor <binder.david.gabor@gmail.com>
2023-05-21 11:29:39 +0200

    Main refactorálása

--- src/com/binderdavid/Main.java
+++ src/com/binderdavid/Main.java
@@ -16,35 +16,47 @@ public class Main {
         boolean veg = false;
 
         while (!veg) {
-           //Nem találkoznak
-           if(karakterek[0].getPozicio() != karakterek[1].getPozicio()){
+
+           if(!karakterekEgyMezonAlnak(karakterek)){
 
                palyaMegjelenito(  palya(karakterek[0], karakterek[1]), karakterek[0], karakterek[1]);
            }
 
 
 
-            //harc
-            if (karakterek[0].getPozicio() == karakterek[1].getPozicio()) {
+
+            if (karakterekEgyMezonAlnak(karakterek)) {
 
 
-                int sebzes = (int) (Math.random() * 6 + 1);
+                int sebzes = 0;
+                try {
+                    sebzes = sebzesD6Kockával();
+                } catch (KarakterNullatSebezKivetel e) {
+                    System.out.println(karakterek[0].getNev()+e.getMessage());
+                    break;
+                }
 
-                if(((Pajzsos)karakterek[1]).ved(sebzes)){
+                if(varazsloVed(sebzes, 1, karakterek)){
 
                     System.out.println("A varázsló kivédte a sebzést");
                 }else{
 
-                    //Harcos sebzi a varázslót
+
                     karakterek[1].serul(sebzes);
                     palyaMegjelenitoHarc(  palya(karakterek[0], karakterek[1]), karakterek[0], karakterek[1]);
                 }
 
-                 //Varázsló sebzi a harcost
-                if (karakterek[1].getEletero() > 0) {
 
-                    int ellenCsapas = (int) (Math.random() * 6 + 1);
-                    if (((Pajzsos)karakterek[0]).ved(sebzes)){
+                if (varazsloEleteNagyobbMintNulla(karakterek[1])) {
+
+                    int ellenCsapas =0;
+                    try {
+                        ellenCsapas = sebzesD6Kockával();
+                    } catch (KarakterNullatSebezKivetel e) {
+                        System.out.println(karakterek[1].getNev()+e.getMessage());
+                        break;
+                    }
+                    if (harcosVed(sebzes, 0, karakterek)){
 
                         System.out.println("A harcos kivédte a sebzést");
                     }else{
@@ -55,15 +67,15 @@ public class Main {
 
 
                 }
-                if (karakterek[0].getEletero() <= 0 || karakterek[1].getEletero() <= 0) {
+                if (MeghaltEValamelyikKarakter(karakterek)) {
                     veg = true;
                     System.out.println("A  játék véget ért! ");
                 }
 
             }
 
-                karakterek[0].lep();
-                karakterek[1].lep();
+            harcosLep(karakterek, 0);
+            varaszloLep(karakterek, 1);
 
             try {
                 Thread.sleep(1000);
@@ -73,6 +85,47 @@ public class Main {
 
 
         }
+    }
+
+    private static void varaszloLep(Karakter[] karakterek, int i) {
+        karakterek[i].lep();
+    }
+
+    private static void harcosLep(Karakter[] karakterek, int i) {
+        karakterek[i].lep();
+    }
+
+    private static boolean MeghaltEValamelyikKarakter(Karakter[] karakterek) {
+        return karakterek[0].getEletero() <= 0 || karakterek[1].getEletero() <= 0;
+    }
+
+    private static boolean varazsloEleteNagyobbMintNulla(Karakter karakter) {
+        return karakter.getEletero() > 0;
+    }
+
+    private static boolean harcosVed(int sebzes, int i, Karakter[] karakterek) {
+        return ((Pajzsos) karakterek[i]).ved(sebzes);
+    }
+
+    private static boolean varazsloVed(int sebzes, int i, Karakter[] karakterek) {
+        return ((Pajzsos) karakterek[i]).ved(sebzes);
+    }
+
+    private static boolean karakterekEgyMezonAlnak(Karakter[] karakterek) {
+        return karakterek[0].getPozicio() == karakterek[1].getPozicio();
+    }
+
+    private static int sebzesD6Kockával () throws KarakterNullatSebezKivetel {
+        int sebzes=(int) (Math.random() * 7 );
+
+
+         if (sebzes==0){
+
+             throw  new KarakterNullatSebezKivetel(" elesett támadás közben és szörnyet halt, a játék véget ért.");
+         }
+
+
+        return  sebzes;
     }
 
     public static String[] palya(Karakter k1, Karakter k2){
