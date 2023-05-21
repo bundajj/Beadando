03cae2b75c2d0b9540e97fcd0fa42dc694eb3f29
Binder Dávid Gábor <binder.david.gabor@gmail.com>
2023-05-21 11:43:36 +0200

    Kód formatálás

--- src/com/binderdavid/Main.java
+++ src/com/binderdavid/Main.java
@@ -8,7 +8,6 @@ public class Main {
         Karakter[] karakterek = jatekTer.getKarakterek();
 
 
-
         run(karakterek);
     }
 
@@ -17,12 +16,10 @@ public class Main {
 
         while (!veg) {
 
-           if(!karakterekEgyMezonAlnak(karakterek)){
+            if (!karakterekEgyMezonAlnak(karakterek)) {
 
-               palyaMegjelenito(  palya(karakterek[0], karakterek[1]), karakterek[0], karakterek[1]);
-           }
-
-
+                palyaMegjelenito(palya(karakterek[0], karakterek[1]), karakterek[0], karakterek[1]);
+            }
 
 
             if (karakterekEgyMezonAlnak(karakterek)) {
@@ -32,38 +29,37 @@ public class Main {
                 try {
                     sebzes = sebzesD6Kockával();
                 } catch (KarakterNullatSebezKivetel e) {
-                    System.out.println(karakterek[0].getNev()+e.getMessage());
+                    System.out.println(karakterek[0].getNev() + e.getMessage());
                     break;
                 }
 
-                if(varazsloVed(sebzes, 1, karakterek)){
+                if (varazsloVed(sebzes, 1, karakterek)) {
 
                     System.out.println("A varázsló kivédte a sebzést");
-                }else{
+                } else {
 
 
                     karakterek[1].serul(sebzes);
-                    palyaMegjelenitoHarc(  palya(karakterek[0], karakterek[1]), karakterek[0], karakterek[1]);
+                    palyaMegjelenitoHarc(palya(karakterek[0], karakterek[1]), karakterek[0], karakterek[1]);
                 }
 
 
                 if (varazsloEleteNagyobbMintNulla(karakterek[1])) {
 
-                    int ellenCsapas =0;
+                    int ellenCsapas = 0;
                     try {
                         ellenCsapas = sebzesD6Kockával();
                     } catch (KarakterNullatSebezKivetel e) {
-                        System.out.println(karakterek[1].getNev()+e.getMessage());
+                        System.out.println(karakterek[1].getNev() + e.getMessage());
                         break;
                     }
-                    if (harcosVed(sebzes, 0, karakterek)){
+                    if (harcosVed(sebzes, 0, karakterek)) {
 
                         System.out.println("A harcos kivédte a sebzést");
-                    }else{
+                    } else {
                         karakterek[0].serul(ellenCsapas);
-                        palyaMegjelenitoHarcEllencsapás(  palya(karakterek[0], karakterek[1]), karakterek[0], karakterek[1]);
+                        palyaMegjelenitoHarcEllencsapás(palya(karakterek[0], karakterek[1]), karakterek[0], karakterek[1]);
                     }
-
 
 
                 }
@@ -115,37 +111,38 @@ public class Main {
         return karakterek[0].getPozicio() == karakterek[1].getPozicio();
     }
 
-    private static int sebzesD6Kockával () throws KarakterNullatSebezKivetel {
-        int sebzes=(int) (Math.random() * 7 );
+    private static int sebzesD6Kockával() throws KarakterNullatSebezKivetel {
+        int sebzes = (int) (Math.random() * 7);
 
 
-         if (sebzes==0){
+        if (sebzes == 0) {
 
-             throw  new KarakterNullatSebezKivetel(" elesett támadás közben és szörnyet halt, a játék véget ért.");
-         }
+            throw new KarakterNullatSebezKivetel(" elesett támadás közben és szörnyet halt, a játék véget ért.");
+        }
 
 
-        return  sebzes;
+        return sebzes;
     }
 
-    public static String[] palya(Karakter k1, Karakter k2){
-         String[]ter=new String[]{"_","_","_"};
-        if(k1.getPozicio()!=k2.getPozicio()){
+    public static String[] palya(Karakter k1, Karakter k2) {
+        String[] ter = new String[]{"_", "_", "_"};
+        if (k1.getPozicio() != k2.getPozicio()) {
 
-            ter[k1.getPozicio()]= k1.getNev();
-            ter[k2.getPozicio()]= k2.getNev();
+            ter[k1.getPozicio()] = k1.getNev();
+            ter[k2.getPozicio()] = k2.getNev();
 
-              return ter;
-        }else{
-            ter[k1.getPozicio()]= "X";
-           return ter;
+            return ter;
+        } else {
+            ter[k1.getPozicio()] = "X";
+            return ter;
         }
 
     }
-    public static  void palyaMegjelenito(String[]palya,Karakter k1,Karakter k2 ){
-          String palyaKiiras="";
+
+    public static void palyaMegjelenito(String[] palya, Karakter k1, Karakter k2) {
+        String palyaKiiras = "";
         for (int i = 0; i < palya.length; i++) {
-            palyaKiiras+=palya[i];
+            palyaKiiras += palya[i];
 
         }
         System.out.println(new StringBuilder()
@@ -159,10 +156,11 @@ public class Main {
                 .append(":")
                 .append(k2.getEletero()).toString());
     }
-    public static  void palyaMegjelenitoHarc(String[]palya,Karakter k1,Karakter k2 ){
-        String palyaKiiras="";
+
+    public static void palyaMegjelenitoHarc(String[] palya, Karakter k1, Karakter k2) {
+        String palyaKiiras = "";
         for (int i = 0; i < palya.length; i++) {
-            palyaKiiras+=palya[i];
+            palyaKiiras += palya[i];
 
         }
         System.out.println(new StringBuilder()
@@ -177,10 +175,11 @@ public class Main {
                 .append(":")
                 .append(k2.getEletero()).toString());
     }
-    public static  void palyaMegjelenitoHarcEllencsapás(String[]palya,Karakter k1,Karakter k2 ){
-        String palyaKiiras="";
+
+    public static void palyaMegjelenitoHarcEllencsapás(String[] palya, Karakter k1, Karakter k2) {
+        String palyaKiiras = "";
         for (int i = 0; i < palya.length; i++) {
-            palyaKiiras+=palya[i];
+            palyaKiiras += palya[i];
 
         }
         System.out.println(new StringBuilder()
