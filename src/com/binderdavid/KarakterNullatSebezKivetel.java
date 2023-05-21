955197e0de54feead7201213efddf17c77ef8e07
Binder Dávid Gábor <binder.david.gabor@gmail.com>
2023-05-21 10:42:05 +0200

    Saját kivétel osztály létrehozása

---
+++ src/com/binderdavid/KarakterNullatSebezKivetel.java
@@ -0,0 +1,7 @@
+package com.binderdavid;
+
+public class KarakterNullatSebezKivetel extends RuntimeException {
+    public KarakterNullatSebezKivetel(String message) {
+        super(message);
+    }
+}
